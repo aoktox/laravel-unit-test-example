7719c985ea66ac5fb959dcb5b0998c4093d9ae38
Agus Prasetiyo <aoktox@users.noreply.github.com>
2017-11-13 16:55:39 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <h2> By codeship and heroku </h2>
             </div>
         </div>
     </body>
